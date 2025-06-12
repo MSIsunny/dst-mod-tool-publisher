38dcea76c77d62af00d1d482738197740ff484c1
荧之空 <2380749795@qq.com>
2025-06-13 01:06:23 +0800

    update page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import logo from './app_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='Title'>
+          dst-mod-tool-v0.1.0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className='Link-container'>
+
+            <a
+            className="App-link"
+            href="https://github.com/MSIsunny/dst-mod-tool-publisher/releases/download/dst-mod-tool-v0.1.0/win-dst-mod-tool.exe.zip"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Windows Download
+            </a>
+
+            <a
+            className="App-link"
+            href="https://github.com/MSIsunny/dst-mod-tool-publisher/releases/download/dst-mod-tool-v0.1.0/macos-dst-mod-tool.zip"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            MacOS Download
+            </a>
+            
+        </div>
       </header>
     </div>
   );
